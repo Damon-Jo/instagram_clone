fb1abb0bb91b152d40bc72d138a71aa1f2383524
Damon-Jo <lunaticjky@gmail.com>
2023-05-26 23:57:56 -0700

    created migrated main page

--- content/views.py
+++ content/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Feed
 
 # Create your views here.
+from rest_framework.views import APIView
+
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all() # select * from content_feed;
+        print(feed_list)
+        return render(request, "jinstagram/main.html")
