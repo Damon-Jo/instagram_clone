e7f838e45196a53e10fd5799f6a074f157cd9105
Damon-Jo <lunaticjky@gmail.com>
2023-06-03 16:22:16 -0700

    fixed error

--- Jinstagram/views.py
+++ Jinstagram/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 class Sub(APIView):
     def get(self, request):
         print("called get")
-        return render(request, "jinstagram/main.html")
+        return render(request, "Jinstagram/main.html")
 
     def post(self, request):
         print("called post")
-        return render(request, "jinstagram/main.html")
+        return render(request, "Jinstagram/main.html")
