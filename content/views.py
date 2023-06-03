4cf0074ae33bdd16541f66ab0a6d110d3e529bc2
Damon-Jo <lunaticjky@gmail.com>
2023-06-02 23:21:49 -0700

    updated the profile page function about like and comment

--- content/views.py
+++ content/views.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from django.shortcuts import render
 from rest_framework.response import Response
 
-from .models import Feed
+from .models import Feed, Reply, Like, Bookmark
 from Jinstagram.settings import MEDIA_ROOT
 from user.models import User
 # Create your views here.
@@ -12,9 +12,7 @@ from rest_framework.views import APIView
 
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed;
 
-        # print('User logged in : ', request.session['email'])
         email = request.session.get('email', None)
 
         if email is None:
@@ -25,12 +23,43 @@ class Main(APIView):
         if user is None:
             return render(request, 'user/login.html')
 
+        feed_object_list = Feed.objects.all().order_by('-id')  # select * from content_feed;
+        feed_list = []
+
+        for feed in feed_object_list:
+            user = User.objects.filter(email=feed.email).first()
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)
+            reply_list = []
+
+            for reply in reply_object_list:
+                user = User.objects.filter(email=reply.email).first()
+                reply_list.append(dict(
+                    reply_content=reply.reply_content,
+                    nickname=user.nickname))
+
+            like_count = Like.objects.filter(feed_id=feed.id, is_like=True).count()
+            is_liked=Like.objects.filter(feed_id=feed.id, email=email, is_like=True).exists()
+
+            feed_list.append(dict(
+                id=feed.id,
+                image=feed.image,
+                content=feed.content,
+                like_count=like_count,
+                profile_image=user.profile_image,
+                nickname=user.nickname,
+                reply_list=reply_list,
+                is_liked = is_liked
+            ))
+
+        # print('User logged in : ', request.session['email'])
+
+
         return render(request, "jinstagram/main.html", context=dict(feeds=feed_list, user=user))
+
 
 class UploadFeed(APIView):
     def post(self, request):
-
-        #at first, read the file
+        # at first, read the file
         file = request.FILES['file']
         uuid_name = uuid4().hex
         save_path = os.path.join(MEDIA_ROOT, uuid_name)
@@ -41,12 +70,12 @@ class UploadFeed(APIView):
 
         image = uuid_name
         content = request.data.get('content')
-        user_id = request.data.get('user_id')
-        profile_image = request.data.get('profile_image')
+        email = request.session.get('email', None)
 
-        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+        Feed.objects.create(image=image, content=content, email=email, like_count=0)
 
         return Response(status=200)
+
 
 class Profile(APIView):
     def get(self, request):
@@ -62,3 +91,33 @@ class Profile(APIView):
             return render(request, 'user/login.html')
 
         return render(request, 'content/profile.html', context=dict(user=user))
+
+
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
+        email = request.session.get('email', None)
+
+        Reply.objects.create(feed_id=feed_id, reply_content=reply_content, email=email)
+
+        return Response(status=200)
+
+
+class ToggleLike(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        favorite_text = request.data.get('favorite_text', None)
+
+        if favorite_text == 'favorite_border':
+            is_like = True
+        else:
+            is_like = False
+
+        email = request.session.get('email', None)
+
+        like = Like.objects.filter(feed_id=feed_id, email=email).first()
+
+        Like.objects.create(feed_id=feed_id, is_like=is_like, email=email)
+
+        return Response(status=200)
